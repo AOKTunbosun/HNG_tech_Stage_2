c191ea00748d3075d98641bb6ec408ce5621b471
AOKTunbosun <aokolatunbosun@gmail.com>
2025-10-28 04:03:19 +0100

    Installed cryptography

--- core_files/database.py
+++ core_files/database.py
@@ -5,11 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST= os.getenv('DB_HOST')
-DB_PORT= os.getenv('DB_PORT')
-DB_USER= os.getenv('DB_USER')
-DB_PASSWORD= os.getenv('DB_PASSWORD')
-DB_NAME= os.getenv('DB_NAME')
 
 # sqlite_file_name = 'database.db'
 # DATABASE_URL = f'sqlite:///{sqlite_file_name}'
